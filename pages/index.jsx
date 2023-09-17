841027749325ff3306a535bf23ff843849f30ca7
alvinfdl21 <alvinfdl21@gmail.com>
2023-09-17 09:59:32 +0700

    fix: user login page

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex flex-col items-center justify-between py-24 ${inter.className}`}
     >
       {/* header */}
       <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
@@ -24,17 +24,17 @@ export default function Home() {
                   Sign Up
                 </button> */}
                     <a
-                      className="w-28 bg-white border-2 border-slate-950 rounded-full px-3 py-2 text-center hover:bg-[#71C40B] hover:border-[#71C40B] hover:text-white hove"
+                      className="w-24 bg-white md:border-2 md:border-slate-950 md:rounded-full px-3 py-2 text-center hover:text-[#71c40b] md:hover:bg-[#71C40B] md:hover:border-[#71C40B] md:hover:text-white hove"
                       to="/sign-in"
                     >
                       Sign In
                     </a>
-                    <a
+                    {/* <a
                       className="w-28 bg-[#71C40B] border-2 border-[#71C40B] hover:bg-white hover:text-[#71C40B]  text-white rounded-full px-3 py-2 text-center"
                       to="/sign-up"
                     >
                       Sign Up
-                    </a>
+                    </a> */}
                   </div>
                 </div>
               </div>
@@ -44,19 +44,34 @@ export default function Home() {
       </header>
       {/* header end */}
       {/* hero start */}
-      <div className="">
-        <h1 className="py-8 text-center font-bold text-5xl">
+      <div className="py-5">
+        <h1 className="text-center font-bold text-5xl">
           Connecting Futures, One Job at a Time.
         </h1>
         <div className="container mx-auto w-11/12 px-10">
           <div className="flex items-center justify-between">
-            <img src="hero-1.svg" alt="" srcSet="" className="w-4/12" />
-            <img src="hero-2.svg" alt="" srcSet="" className="w-4/12" />
-            <img src="hero-3.svg" alt="" srcSet="" className="w-4/12" />
+            <img
+              src="hero-1.svg"
+              alt=""
+              srcSet=""
+              className="hidden md:block md:w-4/12"
+            />
+            <img
+              src="hero-2.svg"
+              alt=""
+              srcSet=""
+              className="w-full md:w-4/12"
+            />
+            <img
+              src="hero-3.svg"
+              alt=""
+              srcSet=""
+              className="hidden md:block md:w-4/12"
+            />
           </div>
         </div>
-        <div className="container mx-auto w-11/12 mt-5 border-b-[1px] border-slate-950"></div>
       </div>
+      <div className="container mx-auto w-11/12 border-b-[1px] border-slate-950"></div>
       {/* hero end */}
     </main>
   );
--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,7 +1,71 @@
 export default function UserLogin() {
   return (
     <>
-      <h1>User Login</h1>
+      <div className="pt-28 py-10">
+        <div className="container mx-auto mb-10">
+          <div className="flex justify-center items-center align-middle w-8/12 mx-auto">
+            <div className="collapse lg:visible">
+              <img src="" alt="" srcSet="" className="w-10/12" />
+            </div>
+            <form className="w-full max-w-md p-4 ">
+              <h2 className="text-2xl mb-4 text-center">Welcome Back</h2>
+              <h2 className="text-xl mb-4 text-center font-light">
+                We are Happy to see you back
+              </h2>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="mt-1 p-2 w-full border-[2px] border-green-200 rounded-2xl focus:ring focus:ring-blue-300"
+                  autoFocus
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="mt-1 p-2 w-full border-[2px] border-green-200 rounded-2xl focus:ring focus:ring-blue-300"
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="text-green-600 py-1 px-4 rounded-2xl w-[100px] hover:bg-green-600 hover:text-white border-2 border-green-600"
+                >
+                  Sign in
+                </button>
+                {/* <Link
+                  className="text-green-600 py-1 px-4 rounded-2xl w-[100px] hover:bg-green-600 hover:text-white  text-center border-2 border-green-600"
+                  to="/index"
+                >
+                  Sign in
+                </Link> */}
+              </div>
+              <p className="text-center pt-3 text-sm font-light">
+                Do not have an account?{" "}
+                {/* <a href="#" className="text-green-600">
+                Sign up
+              </a> */}
+                <a className="text-green-600" to="/sign-up">
+                  Sign Up
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
