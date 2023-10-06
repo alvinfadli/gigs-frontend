044dde09a4c5db07329b5d01a1dd6abeba1b6cb1
alvinfdl21 <alvinfdl21@gmail.com>
2023-10-06 15:20:29 +0700

    update: hr dashboard

--- pages/hr/dashboard/index.jsx
+++ pages/hr/dashboard/index.jsx
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
 import HrNavbar from "@/components/HrNavbar";
 import Sidebar, { SidebarItem } from "@/components/HrSidebar";
 import { hrAuthPage } from "@/middlewares/hrAuth";
+import Insight from "@/components/Dashboard/Insight";
+import ActiveJobs from "@/components/Dashboard/ActiveJobs";
 
 import {
   LayoutDashboard,
@@ -8,8 +11,6 @@ import {
   MonitorDot,
   ScreenShareOff,
 } from "lucide-react";
-import Insight from "@/components/Dashboard/Insight";
-import ActiveJobs from "@/components/Dashboard/ActiveJobs";
 
 export async function getServerSideProps(context) {
   const { token } = await hrAuthPage(context);
@@ -19,6 +20,15 @@ export async function getServerSideProps(context) {
 }
 
 export default function HrDashboard() {
+  const [selectedItem, setSelectedItem] = useState("Insight");
+
+  const handleSidebarItemClick = (text) => {
+    setSelectedItem(text);
+  };
+
+  const isInsightActive = selectedItem === "Insight";
+  const isActiveJobsActive = selectedItem === "Active Jobs";
+
   return (
     <>
       <div className="md:hidden">
@@ -26,18 +36,33 @@ export default function HrDashboard() {
       </div>
       <div className="flex bg-slate-50">
         <Sidebar>
-          <SidebarItem
-            icon={<LayoutDashboard size={20} />}
-            text="Insight"
-            active
-          />
-          <SidebarItem icon={<MonitorDot size={20} />} text="Active Jobs" />
+          <button
+            className={`text-left w-min ${
+              isInsightActive
+                ? "bg-gradient-to-tr from-orange-200 to-orange-100 text-orange-800 rounded-lg"
+                : ""
+            }`}
+            onClick={() => handleSidebarItemClick("Insight")}
+          >
+            <SidebarItem icon={<LayoutDashboard size={20} />} text="Insight" />
+          </button>
+          <button
+            className={`text-left w-min ${
+              isActiveJobsActive
+                ? "bg-gradient-to-tr from-orange-200 to-orange-100 text-orange-800 rounded-lg"
+                : ""
+            }`}
+            onClick={() => handleSidebarItemClick("Active Jobs")}
+          >
+            <SidebarItem icon={<MonitorDot size={20} />} text="Active Jobs" />
+          </button>
           <SidebarItem icon={<ScreenShareOff size={20} />} text="Past Jobs" />
           <hr className="my-3" />
           <SidebarItem icon={<HelpCircle size={20} />} text="Help" />
         </Sidebar>
         <div className=" md:mx-5 mt-20 px-2 md:mt-5 w-full">
-          <Insight />
+          {isInsightActive && <Insight />}
+          {isActiveJobsActive && <ActiveJobs />}
         </div>
       </div>
     </>
